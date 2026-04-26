4eba9da1cfc1443addb1e7adc6e137f6ecd1739d
Vinayagam <iamvinayagam10@gmail.com>
2026-04-26 11:55:06 +0800

    Implement login form and handle POST request

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "MMU Hub Running"
+username = ""
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    global username
+
+    if request.method == "POST":
+        username = request.form.get("username")
+
+        if not username:
+            username = "Guest"
+
+        return redirect("/feed")
+
+    return render_template("login.html")
+
+@app.route("/feed")
+def feed():
+    return "Feed Page"
 
 app.run(debug=True)
