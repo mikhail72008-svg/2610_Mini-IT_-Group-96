c2eff52ba0e3c4741aa22bd6e8e6e3cb20c29c9c
Vinayagam <iamvinayagam10@gmail.com>
2026-05-15 14:07:44 +0800

    Tested Flask template and static file integration

--- app.py
+++ app.py
@@ -1,25 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-username = ""
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def login():
-    global username
-
-    if request.method == "POST":
-        username = request.form.get("username")
-
-        if not username:
-            username = "Guest"
-
-        return redirect("/feed")
-
-    return render_template("login.html")
-
-@app.route("/feed")
-def feed():
-    return render_template("feed.html", username=username)
+    return render_template("Login.html")
 
 app.run(debug=True)
