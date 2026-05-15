13923cee1dec32b4fd5039bfa65e0e601e929854
Vinayagam <iamvinayagam10@gmail.com>
2026-05-15 13:37:33 +0800

    Integrated authentication and posts backend modules

--- test_backend.py
+++ test_backend.py
@@ -1,5 +1,10 @@
 from auth import register_user, login_user
+from posts import create_post, get_all_posts
 
 print(register_user("vinay", "1234"))
 
 print(login_user("vinay", "1234"))
+
+create_post(1, "My first integrated post")
+
+print(get_all_posts())
